4fcead5c27931585253ebf75b45338ccdb6d668f
Coma Grayce <commagray@sunbutt.faith>
2020-11-08 21:57:45 +0200

    Delete useless colorful line.

--- includes/CelestialTemplate.php
+++ includes/CelestialTemplate.php
@@ -61,7 +61,6 @@ class CelestialTemplate extends BaseTemplate {
 			) .
 			$this->getClear()
 		);
-		$html .= $this->getHeaderHack();
 
 		// For mobile
 		$html .= Html::element( 'div', [ 'id' => 'menus-cover' ] );
@@ -606,36 +605,6 @@ class CelestialTemplate extends BaseTemplate {
 		}
 
 		$html = $this->getSidebarChunk( 'site-navigation', 'navigation', $html );
-
-		return $html;
-	}
-
-	/**
-	 * The colour bars
-	 * Split this out so we don't have to look at it/can easily kill it later
-	 *
-	 * @return string html
-	 */
-	protected function getHeaderHack() {
-		$html = '';
-
-		// These are almost exactly the same and this is stupid.
-		$html .= Html::rawElement( 'div', [ 'id' => 'mw-header-hack', 'class' => 'color-bar' ],
-			Html::rawElement( 'div', [ 'class' => 'color-middle-container' ],
-				Html::element( 'div', [ 'class' => 'color-middle' ] )
-			) .
-			Html::element( 'div', [ 'class' => 'color-left' ] ) .
-			Html::element( 'div', [ 'class' => 'color-right' ] )
-		);
-		$html .= Html::rawElement( 'div', [ 'id' => 'mw-header-nav-hack' ],
-			Html::rawElement( 'div', [ 'class' => 'color-bar' ],
-				Html::rawElement( 'div', [ 'class' => 'color-middle-container' ],
-					Html::element( 'div', [ 'class' => 'color-middle' ] )
-				) .
-				Html::element( 'div', [ 'class' => 'color-left' ] ) .
-				Html::element( 'div', [ 'class' => 'color-right' ] )
-			)
-		);
 
 		return $html;
 	}
