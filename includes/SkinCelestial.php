d9cccc91da7ae87859a7e523aa9be5dfd3253bd9
Coma Grayce <commagray@sunbutt.faith>
2020-11-09 08:24:46 +0200

    Responsiveness is kinda broken, so reverting b517e795a85e16443f37eef4dda4d9eeb491a661.

--- includes/SkinCelestial.php
+++ includes/SkinCelestial.php
@@ -6,6 +6,9 @@
  */
 class SkinCelestial extends SkinTemplate {
 	/** @var string */
+	public $skinname = 'celestial';
+
+	/** @var string */
 	public $stylename = 'Celestial';
 
 	/** @var string */
@@ -16,6 +19,11 @@ class SkinCelestial extends SkinTemplate {
 	 */
 	public function initPage( OutputPage $out ) {
 		parent::initPage( $out );
+
+		$out->addMeta( 'viewport',
+			'width=device-width, initial-scale=1.0, ' .
+			'user-scalable=yes, minimum-scale=0.25, maximum-scale=5.0'
+		);
 
 		$out->addModuleStyles( [
 			'mediawiki.skinning.content.externallinks',
